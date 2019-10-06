f7bdbca4dc97205a1e862c77d0161b8946455b3c
prparikh06 <44184103+prparikh06@users.noreply.github.com>
2019-10-05 20:08:09 -0400

    fixed some things

--- mymalloc1.c
+++ mymalloc1.c
@@ -6,7 +6,7 @@ static char myBlock[BLOCKSIZE] = {'\0'};
 char* root = myBlock;
 
 struct metadata{
-    unsigned short isFree: 4;
+    unsigned short isFree: 4; //1 = free, 0 = in use
     unsigned short size: 12;
 }; 
 //short = 2 bytes = 16 bits; partition one short into status (isFree or not) and size of data needed
@@ -23,13 +23,13 @@ char* findMem(unsigned int size){
     while(ptr != NULL){
         currSize = *(int*)(ptr+1);
         printf("currSize compared to size: %d, %d\n", currSize,size);
-        if(*ptr == isFree && currSize >= size){ //+ 1){
+        if(*ptr == isFree && currSize >= size){ 
             //found it
             //memoryLeft -= size+1;
             return ptr;
         }
 	    else{
-		    if(ptr + currSize + size >= root + BLOCKSIZE -1){ //there isnt enough space
+		    if(ptr + currSize + size >= root + BLOCKSIZE - 1){ //there isnt enough space
                 printf("HERE\n");
                 return NULL;
             }
@@ -48,26 +48,26 @@ void split(char* block, unsigned int size){
     int block_size = *(int*)(block+1);
 
     //extra block size
-    int extra_block_size = block_size - size -1;
+    int extra_block_size = block_size - size - 2;
 	if (extra_block_size > 0){
-
-	    extra = block + size + 1;
-	    *extra = isFree;
-	    *(int*)(extra+1) = block_size;
+        struct metadata extraMetadata;
+        extraMetadata->isFree = 1;
+        extraMetadata->size = extra_block_size;
+	    extra = block + size + 2;
+	    *extra = extraMetadata->isFree;
+	    *(int*)(extra+1) = extraMetadata->size;
 	    return;
 	}
 	else{ //idk what to do, there isnt enough space for metadata
-		//TODO can't split, so just allocate the rest of it
 		
+		return;
 	}
- 
-
 }
 
 void* mymalloc(unsigned int size, char* file, int line){
     printf("malloc is called!\n");
     if(size <= 0 || memoryLeft<=size) return NULL;
-    printf("attemping to malloc %d bytes! Therefore we need at least %d bytes. We have about %d left.\n",size,size+1,memoryLeft);
+    printf("attemping to malloc %d bytes! Therefore we need at least %d bytes. We have about %d left.\n",size,size+2,memoryLeft);
 
     //check to see if root is initialized
     if(root[0] == '\0'){ //FIRST USE, MUST INSTANTIATE MEMORY BLOCK
@@ -75,11 +75,10 @@ void* mymalloc(unsigned int size, char* file, int line){
         printf("First use of malloc\n");
         struct metadata rootMetadata;
 		rootMetadata->isFree = 1;
-		rootMetadata->size = BLOCKSIZE -1;
-		*root = isFree;
-
+		rootMetadata->size = BLOCKSIZE -2;
+		*root = rootMetadata->isFree;
         //update the size of the whole block next
-        *(int*)(root+1) = BLOCKSIZE - 1; //1 will be the min metadata we can use 
+        *(int*)(root+1) = rootMetadata->size; //2 will be the min metadata we can use 
     }
 
     //FIND A BLOCK OF MEM WITH GIVEN SIZE
@@ -93,9 +92,10 @@ void* mymalloc(unsigned int size, char* file, int line){
             printf("leftover memory too big, splitting \n");
             split(freeMem,size);
         }
-	    *(freeMem) = inUse;
-	    *(int*) (freeMem+1) = size+1; //initialize the metadata
-        memoryLeft -= size + 1;
+
+	    *(freeMem) = 0;
+	    *(int*) (freeMem+1) = size; //assign the metadata
+        memoryLeft -= size + 2;
         printf("memory left after mallocing: %d\n",memoryLeft);
     }else{
         printf("no memory found\n");
@@ -103,6 +103,5 @@ void* mymalloc(unsigned int size, char* file, int line){
     }
 
     //return the pointer after metadata
-
-    return  freeMem+1;
+    return  freeMem+2;
 }
